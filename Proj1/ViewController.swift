adb26ff6a670cd03918abbe31b0a59b32833f12b
Akram Zaky <akramzaky@Akrams-MacBook-Pro.local>
2016-06-23 19:38:35 -0400

    Initial commit

--- Proj1/ViewController.swift
+++ Proj1/ViewController.swift
@@ -10,16 +10,21 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var btn1: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        btn1.layer.cornerRadius = 10.0
     }
 
+    override func viewDidAppear(animated: Bool) {
+        
+    }
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
 
 }
 
